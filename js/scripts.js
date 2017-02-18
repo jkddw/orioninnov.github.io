6134f24298b1b8c90d876f359e08f3536be54621
Frank Liu <frank.zijie@gmail.com>
2017-02-17 22:16:16 -0800

    Changed company name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
 
 $('body').scrollspy({
-  target: '#navbar-collapsible',
-  offset: 52
+    target: '#navbar-collapsible',
+    offset: 52
 });
 
 
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, 800);
-        
-        /*
-        if (this.hash=="#section1") {
-            $('.scroll-up').hide();
-        }
-        else {
-            $('.scroll-up').show();
-        }
-        */
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 50
+            }, 800);
 
-        return false;
+          return false;
       }
     }
 });
+
+$('#email-btn').click(function() {
+    alert('Oops, something bad happened. Please shoot us an email instead.')
+})
